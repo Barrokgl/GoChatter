7d677a44e6846d95b88bcfb08c8a71b15b4024cd
gkrivonosov <georgykrivon@gmail.com>
2017-01-10 17:25:15 +0300

    added godep

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	port := ":8000"
+	log.Print("Starting server on port: ", port)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(w, "Awesome chat server based on go!")
+	})
+	http.HandleFunc("/ws", wsHandler)
+
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if _, ok := err.(websocket.HandshakeError); ok {
+		http.Error(w, "Not a websocket handshake", 400)
+		return
+	} else if err != nil {
+		return
+	}
+	defer ws.Close()
+	for {
+		messageType, p, err := ws.ReadMessage()
+		if err != nil {
+			panic(err)
+		}
+		if err = ws.WriteMessage(messageType, p); err != nil {
+			panic(err)
+		}
+		println("Data incoming: ", messageType, p)
+	}
+}
